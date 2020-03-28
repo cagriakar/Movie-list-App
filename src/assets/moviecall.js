11c5aa12467b5bd2debc7a08facdfc7b4a8b4f50
cagriakar <55461100+cagriakar@users.noreply.github.com>
2020-03-28 12:36:25 +0300

    last config

--- src/assets/moviecall.js
+++ src/assets/moviecall.js
@@ -2,7 +2,6 @@ async function getMovies(path) {
     try {
         const response = await fetch(path);
         const responseMovies = await response.json();
-        console.log(responseMovies);
         return responseMovies;
     } catch (error) {
         console.log(error);
--- src/config/Config.js
+++ src/config/Config.js
@@ -1,3 +1,3 @@
-export const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+export const API_KEY = '733cf72960e3837a08ca59c1fbbc84ac';
 export const API_MAIN_URL = 'https://api.themoviedb.org/3/';
 export const API_IMAGE_URL = 'https://image.tmdb.org/t/p/';
